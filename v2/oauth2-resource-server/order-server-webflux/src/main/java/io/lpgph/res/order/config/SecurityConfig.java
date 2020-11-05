3c8796e07954152a084996837a6d2718ce4adea6
lpgph <penghui.guo@outlook.com>
2020-11-05 23:20:14 +0800

    check scope

--- v2/oauth2-resource-server/order-server-webflux/src/main/java/io/lpgph/res/order/config/SecurityConfig.java
+++ v2/oauth2-resource-server/order-server-webflux/src/main/java/io/lpgph/res/order/config/SecurityConfig.java
@@ -25,74 +25,86 @@ import java.security.interfaces.RSAPublicKey;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-  @Value("${spring.security.oauth2.resourceserver.jwt.public-key-location}")
-  private RSAPublicKey key;
+    @Value("${spring.security.oauth2.resourceserver.jwt.public-key-location}")
+    private RSAPublicKey key;
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  /** 注意 根据路径匹配 如果路径没有认证改通过 则会直接返回认证失败 */
-  @Bean
-  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-    log.info("\n\n  初始化  \n\n");
-    http.authorizeExchange(
-            exchanges ->
-                exchanges
-                    .pathMatchers("/role")
-                    .access(AuthorityReactiveAuthorizationManager.hasAnyScope("ADMIN"))
-                    //                    .hasAnyRole("ADMIN")
-                    .pathMatchers("/auth")
-                    .access(AuthorityReactiveAuthorizationManager.hasScope("SCOPE_TEST"))
-                    //                    .hasAnyAuthority("SCOPE_TEST")
-                    .anyExchange()
-                    .access(AuthorityReactiveAuthorizationManager.authenticated()))
-        .oauth2ResourceServer(
-            oauth2ResourceServer ->
-                oauth2ResourceServer.jwt(
-                    jwt ->
-                        jwt.jwtDecoder(jwtDecoder())
-                            .jwtAuthenticationConverter(getJwtAuthenticationConverter())))
-        .oauth2Client(oAuth2ClientSpec -> jwtDecoder());
-    return http.build();
-  }
-
-  /**
-   * see
-   * https://docs.spring.io/spring-security/site/docs/5.4.1/reference/html5/#webflux-oauth2resourceserver-jwt-authorization
-   * 手动提取权限属性
-   */
-  public Converter<Jwt, Mono<AbstractAuthenticationToken>> getJwtAuthenticationConverter() {
-    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
-        new JwtGrantedAuthoritiesConverter();
-    jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
-    jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
-    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-    converter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
-    return new ReactiveJwtAuthenticationConverterAdapter(converter);
-  }
-
-  /**
-   * see
-   * https://docs.spring.io/spring-security/site/docs/5.4.1/reference/html5/#oauth2resourceserver-jwt-claimsetmapping
-   * 可以解决不同服务器之间时间差, Claim转换 移除 添加 重命名 合并等
-   */
-  @Bean
-  public ReactiveJwtDecoder jwtDecoder() {
-    //    NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withPublicKey(this.key).build();
-    NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withPublicKey(this.key).build();
-    // 如果服务器之间时间不一致 可以设置时间差来解决
-    //    OAuth2TokenValidator<Jwt> withClockSkew = new DelegatingOAuth2TokenValidator<>(
-    //            new JwtTimestampValidator(Duration.ofSeconds(60)));
-    //    jwtDecoder.setJwtValidator(withClockSkew);
-    // 将 客户端 scope 和 用户 authorities 合并到 authorities
-    /*
-     * 认证服务器中可以设置 client 的 authorities 但是如果用户存在 authorities 时会覆盖 client 的 authorities
-     *  根据 oauth2 手册 只有scope属性 这个属性格式是根据自己的需求定义的 可以写成角色 也可以写成请求路径
-     * see https://oauth.net/2/
+    /**
+     * 注意 根据路径匹配 如果路径没有认证改通过 则会直接返回认证失败
      */
-    jwtDecoder.setClaimSetConverter(new MergeAuthoritiesConverter());
-    return jwtDecoder;
-  }
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        log.info("\n\n  初始化  \n\n");
+        /*
+         * 根据 scope 划分客户端权限的  如资源客户端之间远程调用
+         * 其他的权限根据角色的权限划分
+         * 应该没有我考虑的那么麻烦  之前考虑是先验证客户端权限 然后验证角色权限  似乎过了
+         *
+         */
+        http.authorizeExchange(
+                exchanges ->
+                        exchanges
+                                .pathMatchers("/user/**")
+                                .hasAnyAuthority("SCOPE_USER")
+                                .pathMatchers("/seller/**")
+                                .hasAnyAuthority("SCOPE_SELLER")
+                                .pathMatchers("/buyer/**")
+                                .hasAnyAuthority("SCOPE_BUYER")
+                                .pathMatchers("/rpc/**")
+                                .hasAnyAuthority("SCOPE_RPC")
+                                .pathMatchers("/admin/**")
+                                .access(AuthorityReactiveAuthorizationManager.authenticated())
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(
+                        oauth2ResourceServer ->
+                                oauth2ResourceServer.jwt(
+                                        jwt ->
+                                                jwt.jwtDecoder(jwtDecoder())
+                                                        .jwtAuthenticationConverter(getJwtAuthenticationConverter())))
+                .oauth2Client(oAuth2ClientSpec -> jwtDecoder());
+        return http.build();
+    }
+
+    /**
+     * see
+     * https://docs.spring.io/spring-security/site/docs/5.4.1/reference/html5/#webflux-oauth2resourceserver-jwt-authorization
+     * 手动提取权限属性
+     */
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> getJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+                new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+    }
+
+    /**
+     * see
+     * https://docs.spring.io/spring-security/site/docs/5.4.1/reference/html5/#oauth2resourceserver-jwt-claimsetmapping
+     * 可以解决不同服务器之间时间差, Claim转换 移除 添加 重命名 合并等
+     */
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        //    NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withPublicKey(this.key).build();
+        NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withPublicKey(this.key).build();
+        // 如果服务器之间时间不一致 可以设置时间差来解决
+        //    OAuth2TokenValidator<Jwt> withClockSkew = new DelegatingOAuth2TokenValidator<>(
+        //            new JwtTimestampValidator(Duration.ofSeconds(60)));
+        //    jwtDecoder.setJwtValidator(withClockSkew);
+        // 将 客户端 scope 和 用户 authorities 合并到 authorities
+        /*
+         * 认证服务器中可以设置 client 的 authorities 但是如果用户存在 authorities 时会覆盖 client 的 authorities
+         *  根据 oauth2 手册 只有scope属性 这个属性格式是根据自己的需求定义的 可以写成角色 也可以写成请求路径
+         * see https://oauth.net/2/
+         */
+        jwtDecoder.setClaimSetConverter(new MergeAuthoritiesConverter());
+        return jwtDecoder;
+    }
 }
--- v2/oauth2-resource-server/order-server-webflux/src/main/java/io/lpgph/res/order/security/AuthorityReactiveAuthorizationManager.java
+++ v2/oauth2-resource-server/order-server-webflux/src/main/java/io/lpgph/res/order/security/AuthorityReactiveAuthorizationManager.java
@@ -1,6 +1,5 @@
 package io.lpgph.res.order.security;
 
-import io.lpgph.res.order.json.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.ReactiveAuthorizationManager;
@@ -9,107 +8,52 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.Assert;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * see
  * https://docs.spring.io/spring-security/site/docs/5.3.1.BUILD-SNAPSHOT/reference/html5/#authz-authorities
- * 自定义过滤链 如果通过 authorities 没有匹配到 则通过url匹配 SCOPE 是客户端权限范围 请求链是用户权限 请求链在SCOPE内
+ * 自定义过滤链 如果通过 authorities 没有匹配到 则通过url匹配
+ * SCOPE 是客户端权限范围 请求链是用户权限  请求链在SCOPE内
  *
  * @see org.springframework.security.authorization.AuthorityReactiveAuthorizationManager
  */
 @Slf4j
 public class AuthorityReactiveAuthorizationManager
-    implements ReactiveAuthorizationManager<AuthorizationContext> {
+        implements ReactiveAuthorizationManager<AuthorizationContext> {
 
-  private static final String DEFAULT_SCOPE_PREFIX = "SCOPE_";
-
-  private static final String DEFAULT_AUTHORITY_PREFIX = "AUTH_";
-
-  private final String scopePrefix = DEFAULT_SCOPE_PREFIX;
-
-  private final String authPrefix = DEFAULT_AUTHORITY_PREFIX;
-
-  private final List<String> scopes;
-
-  private AuthorityReactiveAuthorizationManager() {
-    this.scopes = Collections.emptyList();
-  }
-
-  private AuthorityReactiveAuthorizationManager(String... scopes) {
-    this.scopes = Arrays.asList(scopes);
-  }
-
-  private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
-
-  @Override
-  public Mono<AuthorizationDecision> check(
-      Mono<Authentication> authentication, AuthorizationContext object) {
-    // 获取当前请求的路径
-    String uri = object.getExchange().getRequest().getURI().getPath();
-    String method = object.getExchange().getRequest().getMethodValue();
-
-    // 先是客户端认证  客户端认证通过 判断是否有权限
-    // 先判断 客户端认证范围是否存在
-    Mono<Map<Boolean, String>> authorityMap =
-        authentication
-            .filter(Authentication::isAuthenticated)
-            .flatMapIterable(Authentication::getAuthorities)
-            .map(GrantedAuthority::getAuthority)
-            .collectMap(a -> a.startsWith(authPrefix));
-
-    if (scopes.isEmpty()) {
-      return authorityMap
-          .filter(a -> a.containsKey(true))
-          .flatMapIterable(Map::values)
-          .any(a -> match(a.substring(0, authPrefix.length() - 1), uri, method))
-          .map(AuthorizationDecision::new)
-          .defaultIfEmpty(new AuthorizationDecision(false));
+    private AuthorityReactiveAuthorizationManager() {
     }
-    if (authorityMap.subscribe(a -> a.get(true)).isDisposed()) {
-      return authentication
-          .filter(Authentication::isAuthenticated)
-          .flatMapIterable(Authentication::getAuthorities)
-          .map(GrantedAuthority::getAuthority)
-          .any(this.scopes::contains)
-          .map(AuthorizationDecision::new)
-          .defaultIfEmpty(new AuthorizationDecision(false));
+
+    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Mono<AuthorizationDecision> check(
+            Mono<Authentication> authentication, AuthorizationContext object) {
+        // 获取当前请求的路径
+        String uri = object.getExchange().getRequest().getURI().getPath();
+        String method = object.getExchange().getRequest().getMethodValue();
+        return authentication
+                .filter(Authentication::isAuthenticated)
+                .flatMapIterable(Authentication::getAuthorities)
+                .map(GrantedAuthority::getAuthority)
+                .any(a -> match(a, uri, method))
+                .map(AuthorizationDecision::new)
+                .defaultIfEmpty(new AuthorizationDecision(false));
     }
-    return authentication
-        .filter(Authentication::isAuthenticated)
-        .flatMapIterable(Authentication::getAuthorities)
-        .map(GrantedAuthority::getAuthority)
-        .any(this.scopes::contains)
-        .map(AuthorizationDecision::new)
-        .defaultIfEmpty(new AuthorizationDecision(false));
-  }
 
-  private boolean match(String a, String uri, String method) {
-    if (a.split(":").length != 2) return false;
-    String[] auth = a.split(":");
-    return antPathMatcher.match(auth[0], uri) && (method.equals(auth[1]) || "ALL".equals(auth[1]));
-  }
-
-  public static AuthorityReactiveAuthorizationManager authenticated() {
-    return new AuthorityReactiveAuthorizationManager();
-  }
-
-  public static AuthorityReactiveAuthorizationManager hasScope(String scope) {
-    Assert.notNull(scope, "scope cannot be null");
-    return new AuthorityReactiveAuthorizationManager(scope);
-  }
-
-  public static AuthorityReactiveAuthorizationManager hasAnyScope(String... scopes) {
-    Assert.notNull(scopes, "scopes cannot be null");
-    for (String scope : scopes) {
-      Assert.notNull(scope, "scope cannot be null");
+    private boolean match(String a, String uri, String method) {
+        if (a.split(":").length != 2) return false;
+        String[] auth = a.split(":");
+        return antPathMatcher.match(auth[0], uri) && (method.equals(auth[1]) || "ALL".equals(auth[1]));
     }
-    return new AuthorityReactiveAuthorizationManager(scopes);
-  }
+
+    public static AuthorityReactiveAuthorizationManager authenticated() {
+        return new AuthorityReactiveAuthorizationManager();
+    }
+
 }
