0f5d7cb63b690326f3b4e935fa2f1fa1514725b8
lpgph <penghui.guo@outlook.com>
2020-11-09 20:00:33 +0800

    client 保存cookie

--- v2/oauth2-client/client2/src/main/java/io/lpgph/client/HelloController.java
+++ v2/oauth2-client/client2/src/main/java/io/lpgph/client/HelloController.java
@@ -1,15 +1,29 @@
 package io.lpgph.client;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+@AllArgsConstructor
 @RestController
 public class HelloController {
+
+  private final ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
+
+  private final WebClient webClient;
+
   @GetMapping("/hello")
   public String index(
       Model model,
@@ -19,5 +33,26 @@ public class HelloController {
     model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
     model.addAttribute("userAttributes", oauth2User.getAttributes());
     return "index";
+  }
+
+
+  /** 通过帐号密码登录 */
+  @PostMapping("/login")
+  public Mono<Object> login(String username, String password, String clientId) {
+    Mono<ClientRegistration> reactiveRegistration =
+            reactiveClientRegistrationRepository.findByRegistrationId(clientId);
+    ClientRegistration registration = reactiveRegistration.block();
+    if (registration == null) throw new RuntimeException("appName 错误");
+    MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+    map.add("client_id", registration.getClientId());
+    map.add("client_secret", registration.getClientSecret());
+    map.add("grant_type", "password");
+    map.add("username", username);
+    map.add("password", password);
+    return webClient
+            .post()
+            .uri(registration.getProviderDetails().getTokenUri(), map)
+            .exchange()
+            .flatMap((response) -> response.bodyToMono(Object.class));
   }
 }
--- v2/oauth2-client/client2/src/main/java/io/lpgph/client/SecurityConfig.java
+++ v2/oauth2-client/client2/src/main/java/io/lpgph/client/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebFluxSecurity
@@ -19,6 +23,7 @@ public class SecurityConfig {
                                         .anyExchange().authenticated()
                 )
                 .csrf().disable()
+                .cors(spec -> spec.configurationSource(corsConfigurationSource()))
                 .oauth2Login(withDefaults());
 //                .oauth2Client(oAuth2ClientSpec ->
 //                oAuth2ClientSpec.authorizationRequestRepository()
@@ -26,4 +31,23 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3600L);
+        corsConfiguration.addExposedHeader("access-control-allow-methods");
+        corsConfiguration.addExposedHeader("access-control-allow-headers");
+        corsConfiguration.addExposedHeader("access-control-allow-origin");
+        corsConfiguration.addExposedHeader("access-control-max-age");
+        corsConfiguration.addExposedHeader("X-Frame-Options");
+        UrlBasedCorsConfigurationSource configurationSource = new
+                UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return configurationSource;
+    }
 }
